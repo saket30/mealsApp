575577c751b64173b82a96a9e2c28255de0b5dab
saket30 <55984990+saket30@users.noreply.github.com>
2020-08-09 12:45:53 +0530

    Add files via upload

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+import '../models/meal.dart';
+
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoritedMeals;
+
+  FavoritesScreen(this.favoritedMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoritedMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet - start ading some!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoritedMeals.length,
+        itemBuilder: (ctx, index) {
+          final meal = favoritedMeals[index];
+
+          return MealItem(
+            id: meal.id,
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            complexity: meal.complexity,
+            affordability: meal.affordability,
+          );
+        },
+      );
+    }
+  }
+}
